cea4593a24947e2475c6c1638a91510a14cda5e6
Anshul Ojha <anshulojha091@gmail.com>
2025-07-08 10:48:03 +0530

    Importing CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
